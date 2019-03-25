4b7b20306deba19133804c85af749db2adc69bf8
Vitalii Kalaida <nevadskiy@gmail.com>
2019-03-26 00:10:51 +0200

    WIP template

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,10 @@
 let mix = require('laravel-mix');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
 
-mix.sass('src/scss/app.scss', 'app/css');
+mix.sass('src/scss/app.scss', 'public/css');
+
+mix.webpackConfig({
+    plugins: [new LiveReloadPlugin()]
+});
+
+mix.disableSuccessNotifications();
